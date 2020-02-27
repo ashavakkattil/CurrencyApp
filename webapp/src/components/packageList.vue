4245c0610d938e7a53aa28d9c809f7da3c5e7faf
Asha <asha.vakkattil@gmail.com>
2020-02-27 16:21:36 +0530

    Created the homepage
    Add package button,currency drop down and package list table

---
+++ webapp/src/components/packageList.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-data-table :headers="headers" :items="packages" hide-default-footer>
+      <template v-slot:item.action="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      editedIndex: 0,
+      editedItem: null,
+      packages: [
+        {
+          name: "package 1",
+          price: "1000"
+        },
+        {
+          name: "package 2",
+          price: "2000"
+        },
+        {
+          name: "package 3",
+          price: "1400"
+        }
+      ],
+      headers: [
+        { text: "Package Name", value: "name" },
+        { text: "Package price", value: "price" },
+        { text: "Actions", value: "action" }
+      ]
+    };
+  },
+  methods: {
+    editItem(item) {
+      this.editedItem = item;
+      this.editedIndex = 1;
+    }
+  }
+};
+</script>
--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="home"></div>
+  <div>
+    <v-card class="pa-3">
+      <v-row>
+        <v-col>
+          <v-btn color="success" dark>Add Packages</v-btn>
+        </v-col>
+        <v-col cols="3">
+          <v-select
+            label="Select the currency"
+            :items="items"
+            outlined
+            dense
+            v-model="selectedCurrency"
+          ></v-select>
+        </v-col>
+      </v-row>
+    </v-card>
+     <packageList/>
+  </div>
 </template>
 
 <script>
+import packageList from "../components/packageList";
+
 export default {
   name: "Home",
-  components: {}
+  components: { packageList },
+  data() {
+    return {
+      items: ["INR", "USD", "NG"],
+      selectedCurrency: ""
+    };
+  }
 };
 </script>
+<style scoped>
+</style>>
+  
+
