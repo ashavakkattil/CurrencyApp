f7f1571fda381f52c547a0d8b24ef0c989f9a3aa
Asha <asha.vakkattil@gmail.com>
2020-02-27 17:18:07 +0530

    Db connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://localhost:27017/myTrav', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, "Connection Error"));
+db.once('connected', () => {
+    console.log('Connected to Database')
+})
+
+
+app.listen(5000, () => {
+    console.log('Server started at 5000')
+})
